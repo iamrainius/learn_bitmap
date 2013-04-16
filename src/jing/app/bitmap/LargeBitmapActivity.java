a2c2995ec0a3141e11a45469e43fb2cdff278692
rainius <rainius@163.com>
2013-04-16 14:43:39 +0800

    Update

--- src/jing/app/bitmap/LargeBitmapActivity.java
+++ src/jing/app/bitmap/LargeBitmapActivity.java
@@ -1,10 +1,13 @@
 package jing.app.bitmap;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.BitmapFactory.Options;
 import android.net.Uri;
@@ -14,11 +17,13 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class LargeBitmapActivity extends Activity implements OnClickListener {
 	private static final int REQUEST_CODE = 0;
 	Button mPick;
 	ImageView mImageView;
+	TextView mMeta;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +34,7 @@ public class LargeBitmapActivity extends Activity implements OnClickListener {
 		mPick.setOnClickListener(this);
 		
 		mImageView = (ImageView) findViewById(R.id.show_image);
+		mMeta = (TextView) findViewById(R.id.meta);
 	}
 
 	@Override
@@ -44,7 +50,9 @@ public class LargeBitmapActivity extends Activity implements OnClickListener {
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
 			Uri uri = data.getData();
-			mImageView.setImageURI(uri);
+			// mImageView.setImageURI(uri);
+			LoadBitmapTask task = new LoadBitmapTask();
+			task.execute(uri);
 		}
 		
 	}
@@ -53,20 +61,62 @@ public class LargeBitmapActivity extends Activity implements OnClickListener {
 		int mWidth;
 		int mHeight;
 		String mType;
+		Bitmap mBitmap;
 		
 		@Override
 		protected Void doInBackground(Uri... uris) {
-			BitmapFactory.Options options = new Options();
-			options.inJustDecodeBounds = true;
 			try {
-				InputStream is = getContentResolver().openInputStream(uris[0]);
-				
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
+				mBitmap = decodeSampledBitmapFromUri(uris[0], 100, 100);
+			} catch (IOException e) {
 			}
 			return null;
 		}
+
+		@Override
+		protected void onPostExecute(Void result) {
+			mImageView.setImageBitmap(mBitmap);
+		}
 		
+	}
+	
+	public static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+	    // Raw height and width of image
+	    final int height = options.outHeight;
+	    final int width = options.outWidth;
+	    int inSampleSize = 1;
+	
+	    if (height > reqHeight || width > reqWidth) {
+	
+	        // Calculate ratios of height and width to requested height and width
+	        final int heightRatio = Math.round((float) height / (float) reqHeight);
+	        final int widthRatio = Math.round((float) width / (float) reqWidth);
+	
+	        // Choose the smallest ratio as inSampleSize value, this will guarantee
+	        // a final image with both dimensions larger than or equal to the
+	        // requested height and width.
+	        inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+	    }
+	
+	    return inSampleSize;
+	}
+	
+	public Bitmap decodeSampledBitmapFromUri(Uri uri,
+	        int reqWidth, int reqHeight) throws IOException {
+
+	    // First decode with inJustDecodeBounds=true to check dimensions
+	    final BitmapFactory.Options options = new BitmapFactory.Options();
+	    options.inJustDecodeBounds = true;
+	    InputStream is = getContentResolver().openInputStream(uri);
+	    BitmapFactory.decodeStream(is, null, options);
+
+	    // Calculate inSampleSize
+	    options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+	    // Decode bitmap with inSampleSize set
+	    options.inJustDecodeBounds = false;
+	    is = getContentResolver().openInputStream(uri);
+	    return BitmapFactory.decodeStream(is, null, options);
 	}
 	
 }
